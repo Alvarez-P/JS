ebce8d5126a73d060740f2d940765239ca8feeac
Esteban <adolfo@alvarez.com>
2019-01-04 14:18:59 -0600

    Primera prueba

---
+++ funcionObjeto.js
@@ -0,0 +1,19 @@
+
+var p1 = {
+        nombre : 'Adolfo',
+        apellido : 'Alvarez',
+        edad : 21
+    }
+var p2 = {
+        nombre : 'Esteban',
+        apellido :  'Pacheco',
+        edad: 20
+    }
+
+function Saludo(persona){
+    var { nombre }, { edad } = persona
+    console.log(`Hola me llamo ${nombre} y tengo ${edad} años`)
+}
+
+Saludo(p1)
+Saludo(p2)
