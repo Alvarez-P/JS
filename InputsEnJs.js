f3be334e3c73acdbe6e578d3cfaf6e4407bdbaba
Esteban <adolfo@alvarez.com>
2019-01-24 15:23:25 -0600

    Uso de inputs y switch

---
+++ InputsEnJs.js
@@ -0,0 +1,16 @@
+var signo = prompt('¿Cual es tu signo?')
+console.log(signo)
+
+const signoUsuario = signo => {
+    switch (signo){
+        case 'libra':
+            console.log('eres libra')
+            break
+        case 'cancer':
+        case 'cáncer':
+            console.log('eres cancer')
+            break
+        default: 
+            console.log('No eres libra ni cancer')
+    }
+}
